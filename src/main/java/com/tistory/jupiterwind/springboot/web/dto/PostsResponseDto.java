16a82e6af21c09796818e394ffb8fcf0e07183f1
JupiterWind <hasiyara@naver.com>
2021-12-28 10:17:12 +0900

    gradle 최신버전에 맞춤

---
+++ src/main/java/com/tistory/jupiterwind/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.tistory.jupiterwind.springboot.web.dto;
+
+import com.tistory.jupiterwind.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
