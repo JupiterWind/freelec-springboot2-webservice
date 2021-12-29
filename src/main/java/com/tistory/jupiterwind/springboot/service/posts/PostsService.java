acd863f3f0c38550892ecb065a97c041ba50feff
JupiterWind <hasiyara@naver.com>
2021-12-29 11:47:13 +0900

    gradle 최신버전에 맞춤

--- src/main/java/com/tistory/jupiterwind/springboot/service/posts/PostsService.java
+++ src/main/java/com/tistory/jupiterwind/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.tistory.jupiterwind.springboot.service.posts;
 
 import com.tistory.jupiterwind.springboot.domain.posts.Posts;
 import com.tistory.jupiterwind.springboot.domain.posts.PostsRepository;
+import com.tistory.jupiterwind.springboot.web.dto.PostsListResponseDto;
 import com.tistory.jupiterwind.springboot.web.dto.PostsResponseDto;
 import com.tistory.jupiterwind.springboot.web.dto.PostsSaveRequestDto;
 import com.tistory.jupiterwind.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,6 +31,22 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    //(readOnly = true)를 주면 트랙잭션 범위는 유지하되, 조회 기능만 남겨두어 조회속도가 개선되기 때문에,
+    // 등록,수정,삭제 기능이 전혀없는 서비스 메소드에서 사용하는 것을 추천.
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id){
